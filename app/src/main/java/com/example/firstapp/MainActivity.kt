7549a96b5f03eda003aa6df3a7838fe09e329e6a
depp <pumipat.si@rmuti.ac.th>
2020-07-31 19:14:04 +0700

    Fix Menu1

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -12,8 +12,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
 
         val navController = this.findNavController(R.id.myNavHostFragment)
 
